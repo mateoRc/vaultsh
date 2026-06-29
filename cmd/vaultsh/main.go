c9ecd9e43b44ab115af618d83b4cee45d033ac4b
mmateo <mateo.mahmutovic@marketing-corporation.com>
2026-06-29 10:34:00 +0200

    feat: add graceful HTTP server shutdown

--- cmd/vaultsh/main.go
+++ cmd/vaultsh/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/mateom/vaultsh/internal/httpapi"
 	"github.com/mateom/vaultsh/internal/shell"
@@ -17,7 +21,33 @@ func main() {
 	}
 
 	log.Printf("vaultsh listening on %s", server.Addr)
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatal(err)
+
+	serverErrors := make(chan error, 1)
+	go func() {
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	signalContext, stop := signal.NotifyContext(
+		context.Background(),
+		syscall.SIGINT,
+		syscall.SIGTERM,
+	)
+	defer stop()
+
+	select {
+	case err := <-serverErrors:
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+		return
+	case <-signalContext.Done():
+		log.Print("shutting down vaultsh")
+	}
+
+	shutdownContext, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownContext); err != nil {
+		log.Printf("graceful shutdown failed: %v", err)
 	}
 }
