e958150b39f29d14bdede7ac999ca61cec35adb8
mmateo <mateo.mahmutovic@marketing-corporation.com>
2026-06-29 08:51:01 +0200

    add command registry

--- internal/shell/engine.go
+++ internal/shell/engine.go
@@ -7,16 +7,33 @@ type Result struct {
 	ExitCode int
 }
 
-type Engine struct{}
+type commandFunc func() Result
+
+type Engine struct {
+	commands map[string]commandFunc
+}
+
+func New() *Engine {
+	return &Engine{
+		commands: map[string]commandFunc{
+			"help": help,
+		},
+	}
+}
 
 func (e *Engine) Execute(line string) Result {
-	if line != "help" {
+	command, found := e.commands[line]
+	if !found {
 		return Result{
 			Output:   fmt.Sprintf("command not found: %s", line),
 			ExitCode: 127,
 		}
 	}
 
+	return command()
+}
+
+func help() Result {
 	return Result{
 		Output:   "Available commands:\n  help",
 		ExitCode: 0,
