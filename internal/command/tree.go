2544f6e3cae27f796b5a1621e8a1093309700813
mmateo <mateo.mahmutovic@marketing-corporation.com>
2026-06-29 12:23:43 +0200

    feat: add tree command

---
+++ internal/command/tree.go
@@ -0,0 +1,82 @@
+package command
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/mateom/vaultsh/internal/filesystem"
+)
+
+type Tree struct {
+	workingDirectory *filesystem.WorkingDirectory
+}
+
+func NewTree(workingDirectory *filesystem.WorkingDirectory) Tree {
+	return Tree{workingDirectory: workingDirectory}
+}
+
+func (Tree) Name() string {
+	return "tree"
+}
+
+func (Tree) Description() string {
+	return "Print a directory tree"
+}
+
+func (t Tree) Execute(args []string) Result {
+	if len(args) > 1 {
+		return Result{
+			Output:   "usage: tree [path]",
+			ExitCode: 2,
+		}
+	}
+
+	target := "."
+	if len(args) == 1 {
+		target = args[0]
+	}
+
+	node, resolvedPath, err := t.workingDirectory.Resolve(target)
+	if err != nil {
+		return Result{
+			Output:   fmt.Sprintf("tree: %s: %v", target, err),
+			ExitCode: 1,
+		}
+	}
+
+	var output strings.Builder
+	label := node.Name()
+	if target == "." {
+		label = "."
+	} else if resolvedPath == "/" {
+		label = "/"
+	}
+	output.WriteString(label)
+	writeChildren(&output, node, "")
+
+	return Result{
+		Output:   output.String(),
+		ExitCode: 0,
+	}
+}
+
+func writeChildren(output *strings.Builder, node filesystem.Node, prefix string) {
+	directory, ok := node.(*filesystem.Directory)
+	if !ok {
+		return
+	}
+
+	children := directory.Children()
+	for index, child := range children {
+		last := index == len(children)-1
+		connector := "├── "
+		childPrefix := prefix + "│   "
+		if last {
+			connector = "└── "
+			childPrefix = prefix + "    "
+		}
+
+		fmt.Fprintf(output, "\n%s%s%s", prefix, connector, child.Name())
+		writeChildren(output, child, childPrefix)
+	}
+}
